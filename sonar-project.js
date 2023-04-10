7a7dc2f63b0180f2ed1085970655bbb024bab037
AyobamiOjoibukun <122585526+AyobamiOjoibukun@users.noreply.github.com>
2023-04-09 22:24:35 -0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,13 +1,13 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://54.172.84.124:9000/',
+  serverUrl: 'http://172.31.22.20:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': '7d0ead5572b74b9242a64dc8b3bd1992b04943eb',
-	    //'sonar.password': 'admin',
+	    'sonar.login': 'admin',
+	    'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
